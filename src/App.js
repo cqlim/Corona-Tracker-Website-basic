08d5dd6017b190f672e51c0664b9fe6baa6785d1
flazzing <limchengqing@gmail.com>
2020-05-09 02:06:56 -0700

    added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [latest, setLatest] = useState("");
+	useEffect(() => {
+		axios
+			.get("https://covidtracking.com/api/states/")
+			.then((res) => {
+				setLatest(res.data[37]);
+				console.log(res.data[37]);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<div>
+			<CardDeck>
+				<Card
+					bg="secondary"
+					text="white"
+					className="text=center"
+					style={{ margin: "10px" }}
+				>
+					<Card.Img variant="top" src="holder.js/100px" />
+					<Card.Body>
+						<Card.Title>Cases</Card.Title>
+						<Card.Text>{latest.positive} </Card.Text>
+					</Card.Body>
+				</Card>
+				<Card
+					bg="secondary"
+					text="white"
+					className="text=center"
+					style={{ margin: "10px" }}
+				>
+					<Card.Img variant="top" src="holder.js/100px160" />
+					<Card.Body>
+						<Card.Title>Deaths</Card.Title>
+						<Card.Text>{latest.death}</Card.Text>
+					</Card.Body>
+				</Card>
+				<Card
+					bg="secondary"
+					text="white"
+					className="text=center"
+					style={{ margin: "10px" }}
+				>
+					<Card.Img variant="top" src="holder.js/100px160" />
+					<Card.Body>
+						<Card.Title>Recovered</Card.Title>
+						<Card.Text>{latest.recovered}</Card.Text>
+					</Card.Body>
+				</Card>
+			</CardDeck>
+		</div>
+	);
 }
 
 export default App;
