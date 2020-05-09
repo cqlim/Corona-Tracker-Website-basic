4a4ced47eb4c2d5e0c0460cc4a718e72259db0c8
flazzing <limchengqing@gmail.com>
2020-05-09 05:09:30 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Col from "react-bootstrap/Col";
 import axios from "axios";
+import "./app.css";
+import NumberFormat from "react-number-format";
 
-function App() {
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const App = () => {
 	const [latest, setLatest] = useState("");
+	const [federalLatest, federalsetLatest] = useState("");
+
 	useEffect(() => {
 		axios
 			.get("https://covidtracking.com/api/states/")
 			.then((res) => {
 				setLatest(res.data[37]);
-				console.log(res.data[37]);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+
+		axios
+			.get("http://covidtracking.com/api/us")
+			.then((res) => {
+				federalsetLatest(res.data[0]);
 			})
 			.catch((err) => {
 				console.log(err);
@@ -19,47 +38,183 @@ function App() {
 	}, []);
 
 	return (
-		<div>
-			<CardDeck>
-				<Card
-					bg="secondary"
-					text="white"
-					className="text=center"
-					style={{ margin: "10px" }}
-				>
-					<Card.Img variant="top" src="holder.js/100px" />
-					<Card.Body>
-						<Card.Title>Cases</Card.Title>
-						<Card.Text>{latest.positive} </Card.Text>
-					</Card.Body>
-				</Card>
-				<Card
-					bg="secondary"
-					text="white"
-					className="text=center"
-					style={{ margin: "10px" }}
-				>
-					<Card.Img variant="top" src="holder.js/100px160" />
-					<Card.Body>
-						<Card.Title>Deaths</Card.Title>
-						<Card.Text>{latest.death}</Card.Text>
-					</Card.Body>
-				</Card>
-				<Card
-					bg="secondary"
-					text="white"
-					className="text=center"
-					style={{ margin: "10px" }}
-				>
-					<Card.Img variant="top" src="holder.js/100px160" />
-					<Card.Body>
-						<Card.Title>Recovered</Card.Title>
-						<Card.Text>{latest.recovered}</Card.Text>
-					</Card.Body>
-				</Card>
-			</CardDeck>
+		<div className="out-div">
+			<Container>
+				<h2>Status of Oregon</h2>>
+				<Row>
+					<Col>
+						<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Flag_of_Oregon.svg/2000px-Flag_of_Oregon.svg.png" />
+					</Col>
+				</Row>
+				<Row>
+					<CardDeck style={{ margin: "50px" }}>
+						<Col xs={6}>
+							<Card bg="secondary" text="white" style={{ margin: "10px" }}>
+								<Card.Header>Cases</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={latest.positive}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col>
+							<Card
+								bg="danger"
+								text="white"
+								className="text=center"
+								style={{ margin: "10px" }}
+							>
+								<Card.Header>Deaths</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={latest.death}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col xs={6}>
+							<Card
+								bg="success"
+								text="white"
+								className="text=center"
+								style={{ margin: "10px" }}
+							>
+								<Card.Header>Recovered</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={latest.recovered}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col xs={6}>
+							<Card
+								bg="info"
+								text="white"
+								className="text=center"
+								style={{ margin: "10px" }}
+							>
+								<Card.Header>Currently Hospitalized</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={latest.hospitalizedCurrently}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+					</CardDeck>
+				</Row>
+			</Container>
+			<Container>
+				<h2>Status of United State</h2>>
+				<Row>
+					<Col>
+						<img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png" />
+					</Col>
+				</Row>
+				<Row>
+					<CardDeck style={{ margin: "50px" }}>
+						<Col xs={6}>
+							<Card bg="secondary" text="white" style={{ margin: "10px" }}>
+								<Card.Header>Total tested results</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={federalLatest.totalTestResults}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col>
+							<Card
+								bg="danger"
+								text="white"
+								className="text=center"
+								style={{ margin: "10px" }}
+							>
+								<Card.Header>Total tested positives</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={federalLatest.positive}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col xs={6}>
+							<Card
+								bg="success"
+								text="white"
+								className="text=center"
+								style={{ margin: "10px" }}
+							>
+								<Card.Header>Total tested negatives</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={federalLatest.negative}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col xs={6}>
+							<Card
+								bg="info"
+								text="white"
+								className="text=center"
+								style={{ margin: "10px" }}
+							>
+								<Card.Header>Total test pending</Card.Header>
+								<Card.Body>
+									<Card.Title>
+										<NumberFormat
+											value={federalLatest.pending}
+											displayType={"text"}
+											thousandSeparator={true}
+											prefix={"Total: "}
+										/>
+									</Card.Title>
+								</Card.Body>
+							</Card>
+						</Col>
+					</CardDeck>
+				</Row>
+			</Container>
 		</div>
 	);
-}
+};
 
 export default App;
