164420fd6bd7a43f89a3e0669778b4169600f27a
flazzing <limchengqing@gmail.com>
2020-05-09 05:17:08 -0700

    Updates

---
+++ precache-manifest.8a73c502f389d67eb4e83a9d376acc18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d252b0546cd6805213a9d351c299748e",
+    "url": "/Corona-Tracker-Website/index.html"
+  },
+  {
+    "revision": "35bc4c5b7ad1d1586691",
+    "url": "/Corona-Tracker-Website/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1d72f565c126e6096dcd",
+    "url": "/Corona-Tracker-Website/static/css/main.816cec7c.chunk.css"
+  },
+  {
+    "revision": "35bc4c5b7ad1d1586691",
+    "url": "/Corona-Tracker-Website/static/js/2.36abfb26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Corona-Tracker-Website/static/js/2.36abfb26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d72f565c126e6096dcd",
+    "url": "/Corona-Tracker-Website/static/js/main.34dc995f.chunk.js"
+  },
+  {
+    "revision": "f5b6455a297d38ae1195",
+    "url": "/Corona-Tracker-Website/static/js/runtime-main.e43a5c3c.js"
+  }
+]);
